f151a50a335047513223e11bfb66ddabe967702f
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-07-13 22:40:43 +0900

    코드 베이스로 설정

--- KnockKnock/KnockKnock/RoomViewController.swift
+++ KnockKnock/KnockKnock/RoomViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RoomViewController: UIViewController {
 
+    let button = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(button)
+        button.
     }
 
 
