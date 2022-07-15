538dd4220ad841c11da42de0ceb36139ee6f1455
Gold-MineScore <96890040+DevMizeKR@users.noreply.github.com>
2022-07-15 15:21:36 +0900

    feature: CollectionView 추가

---
+++ KnockKnock/KnockKnock/Album/AlbumCollectionViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  AlbumCollectionViewCell.swift
+//  KnockKnock
+//
+//  Created by HWANG-C-K on 2022/07/15.
+//
+
+import UIKit
+
+//CollectionView의 Cell 구성
+final class albumImageCell: UICollectionViewCell {
+    static let id = "albumImageCell"
+    
+    //Cell에 ImageView 추가
+    let imageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .gray
+        view.contentMode = .scaleToFill
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    @available(*, unavailable)
+    required init?(coder:NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame:CGRect){
+        super.init(frame:frame)
+        
+        //Cell 내에서 ImageView의 위치 지정
+        self.contentView.addSubview(self.imageView)
+        NSLayoutConstraint.activate([
+              self.imageView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor),
+              self.imageView.rightAnchor.constraint(equalTo: self.contentView.rightAnchor),
+              self.imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+              self.imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor)
+        ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.prepare(image:nil)
+    }
+    
+    func prepare(image: UIImage?){
+        self.imageView.image = image
+    }
+}
--- KnockKnock/KnockKnock/Album/MainAlbumViewController.swift
+++ KnockKnock/KnockKnock/Album/MainAlbumViewController.swift
@@ -12,6 +12,29 @@ class MainAlbumViewController: UIViewController {
     
     var itemProviders: [NSItemProvider] = []
     var imageArray: [UIImage] = []
+    
+    //CollectionView 기본 설정
+    private let gridFlowLayout : UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 2
+        layout.minimumInteritemSpacing = 2
+        layout.scrollDirection = .vertical
+        return layout
+    }()
+    
+    //CollectionView 정의
+    private lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame:.zero, collectionViewLayout: self.gridFlowLayout)
+        view.isScrollEnabled = true
+        view.showsHorizontalScrollIndicator = false
+        view.showsVerticalScrollIndicator = true
+        view.contentInset = .zero
+        view.backgroundColor = .gray
+        view.clipsToBounds = true
+        view.register(albumImageCell.self, forCellWithReuseIdentifier: "albumImageCell")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     //ImagePicker 함수
     @objc func presentPicker(_ sender: Any) {
@@ -29,13 +52,21 @@ class MainAlbumViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(collectionView)
         
         //NavigationBar에 ImagePicker 버튼 생성
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(presentPicker))
         
+        //CollectionView AutoLayout
+        NSLayoutConstraint.activate([
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-    
-
     /*
     // MARK: - Navigation
 
@@ -60,9 +91,37 @@ extension MainAlbumViewController: PHPickerViewControllerDelegate {
                     DispatchQueue.main.async {
                         guard let image = image as? UIImage else { return }
                         self.imageArray.append(image)
+                        self.collectionView.reloadData()
                     }
                 }
             }
         }
     }
+}
+
+extension MainAlbumViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    //CollectionView Cell의 Size 지정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
+        guard
+            let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else{fatalError()}
+        
+        let cellColumns = 3
+        let widthOfCells = collectionView.bounds.width
+        let widthOfSpacing = CGFloat(cellColumns - 1) * flowLayout.minimumInteritemSpacing
+        let width = (widthOfCells-widthOfSpacing) / CGFloat(cellColumns)
+
+        return CGSize(width: width, height: width)
+    }
+    
+    //CollectionView에 표시되는 Item의 수
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.imageArray.count
+    }
+    
+    //CollectionView의 각 cell에 이미지 표시
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: albumImageCell.id, for: indexPath) as! albumImageCell
+        cell.prepare(image: self.imageArray[indexPath.item])
+        return cell
+      }
 }
