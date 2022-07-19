b88782df7c065b49df6a76a7980d32d1b5892ceb
jung <go90js@gmail.com>
2022-07-19 15:33:10 +0900

    feat: add Task Model

--- KnockKnock/KnockKnock/PasscodeSetting/SettingViewController.swift
+++ KnockKnock/KnockKnock/PasscodeSetting/SettingViewController.swift
@@ -8,22 +8,67 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+    
+    var tasks = [Task(title: "화면 잠금", isSwitch: true, isSwitchOn: false)]
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "비밀번호 설정"
+        view.backgroundColor = .red
+        self.tableViewSetup()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func tableViewSetup() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        NSLayoutConstraint.activate([
+            
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        ])
     }
-    */
 
+
+}
+
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(tasks.count)
+        return self.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let switchView = UISwitch(frame: .zero)
+        switchView.setOn(task.isSwitchOn, animated: true)
+                                switchView.tag = indexPath.row
+                                switchView.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
+        if task.isSwitch {
+            cell.accessoryView = switchView
+        } else {
+            cell.accessoryView = nil
+        }
+                                
+        
+        return cell
+    }
+    
 }
