400736684e8b43c818f70452b6d50e46c669f660
dasol <hurdasol@naver.com>
2022-07-19 15:49:45 +0900

    [ADD] 앨범피커이미지 코어데이터 구현

--- KnockKnock/CoreData/CoreDataManager.swift
+++ KnockKnock/CoreData/CoreDataManager.swift
@@ -95,5 +95,55 @@ class CoreDataManager {
             return false
         }
     }
+    // MARK: - ALBUM
+    var albumImageArray: [NSManagedObject]? = []
+    //[CREATE]
+    func saveAlbumCoreData(image: Data) {
+        // App Delegate 호출
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        // App Delegate 내부에 있는 viewContext 호출
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // managedContext 내부에 있는 entity 호출
+        let entity = NSEntityDescription.entity(forEntityName: "Album", in: managedContext)!
+        
+        // entity 객체 생성
+        let object = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        // 값 설정
+        object.setValue(Date(), forKey: "date")
+        object.setValue(UUID(), forKey: "id")
+        object.setValue(image, forKey: "image")
+        
+        do {
+            // managedContext 내부의 변경사항 저장
+            try managedContext.save()
+        } catch let error as NSError {
+            // 에러 발생시
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    //[READ]
+    func readAlbumCoreData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // Entity의 fetchRequest 생성
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Album")
+        
+        do {
+            // fetchRequest를 통해 managedContext로부터 결과 배열을 가져오기
+            let resultCDArray = try managedContext.fetch(fetchRequest)
+            self.albumImageArray = resultCDArray
+
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    
+    
+    
     
 }
