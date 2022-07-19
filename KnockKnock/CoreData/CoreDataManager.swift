6b35b1ff4216f0e4e11e69756e217dab7db3d661
dasol <hurdasol@naver.com>
2022-07-20 04:31:18 +0900

    [ADD] 코어데이터 Frame Entity 추가 및 코어데이터 매니저 기능 추가

--- KnockKnock/CoreData/CoreDataManager.swift
+++ KnockKnock/CoreData/CoreDataManager.swift
@@ -13,7 +13,8 @@ class CoreDataManager {
     static let shared = CoreDataManager()
     
     var resultArray: [NSManagedObject]?
-    var albumImageArray: [NSManagedObject]? = []
+    var albumImageArray: [NSManagedObject]?
+    var frameImage: [NSManagedObject]?
     
     //MARK: - 메모 부분 CoreData
     
@@ -166,5 +167,64 @@ class CoreDataManager {
         }
     }
     
+    // MARK: - 액자 부분 coreData
+    //[CREATE]
+    func saveFrameCoreData(image: Data) {
+        // App Delegate 호출
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        // App Delegate 내부에 있는 viewContext 호출
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // managedContext 내부에 있는 entity 호출
+        let entity = NSEntityDescription.entity(forEntityName: "Frame", in: managedContext)!
+        
+        // entity 객체 생성
+        let object = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        // 값 설정
+        object.setValue(UUID(), forKey: "id")
+        object.setValue(image, forKey: "image")
+        
+        do {
+            // managedContext 내부의 변경사항 저장
+            try managedContext.save()
+        } catch let error as NSError {
+            // 에러 발생시
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
     
+    //[READ]
+    func readFrameCoreData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // Entity의 fetchRequest 생성
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Frame")
+        
+        do {
+            // fetchRequest를 통해 managedContext로부터 결과 배열을 가져오기
+            let resultCDArray = try managedContext.fetch(fetchRequest)
+            self.frameImage = resultCDArray
+
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    //[DELETE]
+    func deleteFrameCoreData(object: NSManagedObject) -> Bool {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return false }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // 객체를 넘기고 바로 삭제
+        managedContext.delete(object)
+        do {
+            try managedContext.save()
+            return true
+        } catch let error as NSError {
+            print("Could not update. \(error), \(error.userInfo)")
+            return false
+        }
+    }
 }
