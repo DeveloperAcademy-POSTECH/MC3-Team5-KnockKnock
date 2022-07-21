415f156ae3909e2f29239e1c90eeb5f1d8e319cf
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-07-21 16:31:09 +0900

    ㅇㅇ
    ㅇㅇ

--- KnockKnock/KnockKnock/RoomViewController.swift
+++ KnockKnock/KnockKnock/RoomViewController.swift
@@ -213,6 +213,8 @@ class RoomViewController: UIViewController {
         if let image = CoreDataManager.shared.frameImage?.last {
             if let frameImageData = image.value(forKey: "image") as? Data {
                 frameHasImageView.image = UIImage(data: frameImageData)
+                //테스트
+                print("해겨ㅑㄹ")
             }
         }
     }
