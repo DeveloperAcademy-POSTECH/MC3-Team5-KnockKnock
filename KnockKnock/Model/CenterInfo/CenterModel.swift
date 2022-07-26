b115597406ae7e9392c5f64852ae087ea9ab900a
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2022-07-26 18:32:15 +0900

    add : #23 센터 데이터 모델 추가

---
+++ KnockKnock/Model/CenterInfo/CenterModel.swift
@@ -0,0 +1,20 @@
+//
+//  CenterModel.swift
+//  KnockKnock
+//
+//  Created by LeeJaehoon on 2022/07/25.
+//
+
+import Foundation
+
+struct CenterModel: Codable {
+    let name: String
+    let address: String
+    let tel: String
+    let gps: GpsInfo
+}
+
+struct GpsInfo: Codable {
+    let latitude: Double // 위도
+    let longitude: Double // 경도
+}
